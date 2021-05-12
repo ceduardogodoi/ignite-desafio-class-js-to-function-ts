bdd02290497eaa178dbc5ec1c84673e9ea5e59bf
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-05-12 19:27:42 -0300

    changes some components to function ones

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,71 @@
+import { Component, useState } from "react";
+import ReactModal from "react-modal";
+
+interface ModalProps {
+  isOpen: boolean;
+  setIsOpen(): void;
+}
+
+interface ModalState {
+  modalStatus: boolean;
+}
+
+function ModalComponent({ isOpen, setIsOpen }: ModalProps) {
+  const [modalStatus, setModalStatus] = useState(isOpen);
+}
+
+class Modal extends Component<ModalProps, ModalState> {
+  constructor(props: ModalProps) {
+    super(props);
+
+    const { isOpen } = this.props;
+
+    this.state = {
+      modalStatus: isOpen,
+    };
+  }
+
+  componentDidUpdate(prevProps: ModalProps) {
+    const { isOpen } = this.props;
+
+    if (prevProps.isOpen !== isOpen) {
+      this.setState({ modalStatus: isOpen });
+    }
+  }
+
+  render() {
+    const { children, setIsOpen } = this.props;
+    const { modalStatus } = this.state;
+
+    return (
+      <ReactModal
+        shouldCloseOnOverlayClick={!false}
+        onRequestClose={setIsOpen}
+        isOpen={modalStatus}
+        ariaHideApp={false}
+        style={{
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+            background: "#F0F0F5",
+            color: "#000000",
+            borderRadius: "8px",
+            width: "736px",
+            border: "none",
+          },
+          overlay: {
+            backgroundColor: "#121214e6",
+          },
+        }}
+      >
+        {children}
+      </ReactModal>
+    );
+  }
+}
+
+export default Modal;
